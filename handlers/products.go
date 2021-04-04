07b063be7cc68a86548446afe621b50ba52f538b
rasimthegrey <rasim.mutlu@protonmail.com>
2021-04-05 00:44:04 +0300

    code is refactored by using gorilla

--- handlers/products.go
+++ handlers/products.go
@@ -1,104 +1,57 @@
 package handlers
 
 import (
+	"context"
+	"github.com/gorilla/mux"
 	"log"
 	"microservices-with-go/data"
 	"net/http"
-	"regexp"
 	"strconv"
 )
 
-// Products is a http.Handler
+/// Products is a http.Handler
 type Products struct {
 	l *log.Logger
 }
 
-//NewProducts creates a new products handler with the given logger
+// NewProducts creates a products handler with the given logger
 func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
-//ServeHTTP is the main entry point for the handler and satisfies the http.Handler interface
-func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request){
-	// handle the request for a list of products
-	if r.Method == http.MethodGet {
-		p.getProducts(rw, r)
-		return
-	}
-	//handle an update
-	if r.Method == http.MethodPost {
-		p.addProduct(rw, r)
-		return
-	}
-
-	if r.Method == http.MethodPut {
-		// expect the id in the URI
-		reg := regexp.MustCompile(`/([0-9]+)`)
-		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
-
-		if len(g) != 1 {
-			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-			return
-		}
-
-		if len(g[0]) != 2 {
-			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-			return
-		}
-
-		idString := g[0][1]
-		id, err := strconv.Atoi(idString)
-		if err != nil {
-			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-			return
-		}
-
-		p.updateProducts(id, rw, r)
-		return
-	}
-
-	//catch all
-	// if no method is satisfied return an error
-	rw.WriteHeader(http.StatusMethodNotAllowed)
-}
-
 // getProducts returns the products from the data store
-func (p *Products) getProducts(rw http.ResponseWriter, h *http.Request) {
+func (p *Products) GetProducts(rw http.ResponseWriter, r *http.Request) {
 	p.l.Println("Handle GET Products")
 
-	//fetch the products
-	lp := data.GetProducts() //lp: list of products
+	// fetch the products from the datastore
+	lp := data.GetProducts()
 
-	//serialize the list to JSON
+	// serialize the list to JSON
 	err := lp.ToJSON(rw)
 	if err != nil {
-		http.Error(rw, "unable to marshal json", http.StatusInternalServerError)
+		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
 	}
 }
 
-func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request){
-	p.l.Println("Handle POST Products")
+func (p *Products) AddProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST Product")
 
-	prod := &data.Product{}
-	err := prod.FromJSON(r.Body)
-	if err != nil{
-		http.Error(rw, "unable to unmarshal json", http.StatusBadRequest)
-	}
-
-	data.AddProduct(prod)
+	prod := r.Context().Value(KeyProduct{}).(data.Product)
+	data.AddProduct(&prod)
 }
 
-func (p Products) updateProducts(id int, rw http.ResponseWriter, r *http.Request){
-	p.l.Println("Handle PUT Products")
-
-	prod := &data.Product{}
-
-	err := prod.FromJSON(r.Body)
-	if err != nil{
-		http.Error(rw, "unable to unmarshal json", http.StatusBadRequest)
+func (p Products) UpdateProducts(rw http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(rw, "Unable to convert id", http.StatusBadRequest)
+		return
 	}
 
-	err = data.UpdateProduct(id, prod)
+	p.l.Println("Handle PUT Product", id)
+	prod := r.Context().Value(KeyProduct{}).(data.Product)
+
+	err = data.UpdateProduct(id, &prod)
 	if err == data.ErrProductNotFound {
 		http.Error(rw, "Product not found", http.StatusNotFound)
 		return
@@ -108,4 +61,26 @@ func (p Products) updateProducts(id int, rw http.ResponseWriter, r *http.Request
 		http.Error(rw, "Product not found", http.StatusInternalServerError)
 		return
 	}
+}
+
+type KeyProduct struct{}
+
+func (p Products) MiddlewareValidateProduct(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		prod := data.Product{}
+
+		err := prod.FromJSON(r.Body)
+		if err != nil {
+			p.l.Println("[ERROR] deserializing product", err)
+			http.Error(rw, "Error reading product", http.StatusBadRequest)
+			return
+		}
+
+		// add the product to the context
+		ctx := context.WithValue(r.Context(), KeyProduct{}, prod)
+		r = r.WithContext(ctx)
+
+		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		next.ServeHTTP(rw, r)
+	})
 }
